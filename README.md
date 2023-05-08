1ba1f16e81e819a75781668cb6c7a42810f10203
touheedk <touheedkhan93@gmail.com>
2023-05-08 09:43:28 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Vue3-DataTables-Bootstrap5
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/datatables-net-vue3-bootstrap5-nqahqy)
---
+++ src/App.vue
@@ -0,0 +1,62 @@
+<script setup lang="ts">
+import DataTable from 'datatables.net-vue3';
+import DataTablesCore from 'datatables.net-bs5';
+
+DataTable.use(DataTablesCore);
+const data = [
+  {
+    id: '1',
+    name: 'Touheed Khan',
+    position: 'Senior Developer',
+    salary: '$320,800',
+    office: 'Mumbai',
+  },
+  {
+    id: '2',
+    name: 'Alex Max',
+    position: 'Manger',
+    salary: '$470,750',
+    office: 'Paris',
+  },
+  {
+    id: '3',
+    name: 'Amar Arjun',
+    position: 'Technical Author',
+    salary: '$286,000',
+    office: 'Berlin',
+  },
+];
+
+const columns = [
+  { data: 'name' },
+  { data: 'position' },
+  { data: 'office' },
+  { data: 'salary' },
+];
+</script>
+
+<template>
+  <div class="container">
+    <h2>Employee List</h2>
+    <DataTable
+      :columns="columns"
+      :data="data"
+      class="table table-hover table-striped"
+      width="100%"
+    >
+      <thead>
+        <tr>
+          <th>Name</th>
+          <th>Position</th>
+          <th>Office</th>
+          <th>Salary</th>
+        </tr>
+      </thead>
+    </DataTable>
+  </div>
+</template>
+
+<style>
+@import 'bootstrap';
+@import 'datatables.net-bs5';
+</style>
